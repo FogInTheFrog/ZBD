bb6732a56565e9171beb2af68793638ca020eb7d
Jakub Zacharczuk <zacharczuk.jakub2k@gmail.com>
2021-12-15 14:26:11 +0100

    update names

--- 04 Python + PostgreSQL/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from database import get_db
-import crud
-
-app = FastAPI()
-
-if __name__ == '__main__':
-    print(crud.get_shortest_path(get_db(), 12126782, 12132897))
